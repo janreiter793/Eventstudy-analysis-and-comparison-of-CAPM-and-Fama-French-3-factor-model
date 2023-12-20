10c430c3e058d819962ac1b009d298f00e5003a7
realKristian <70947933+realKristian@users.noreply.github.com>
2023-12-20 10:41:28 +0100

    Update EventStudyTSLA.R

--- EventStudyTSLA.R
+++ EventStudyTSLA.R
@@ -20,7 +20,7 @@ PERIOD <- c('2002-01-01', '2023-10-31')
 
 # Path to the FF-3 factors
 # Remember to fill in path to the file containing the factors
-PATH <- "C:\\Users\\Krist\\OneDrive - Aalborg Universitet\\AAU\\Semester 7\\project\\EventStudy\\FF3Factors311023.CSV"
+PATH <- "C:\\...\\factors.CSV"
 
 ########## LOAD FF3 FACTORS INTO R ##########
 # Read the factors Rm - Rf, SMB, HML, Rf
